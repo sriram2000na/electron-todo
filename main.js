5a840be2da48235a12237fab1b141a5f1d3032e4
sriram <sriramna18102@it.ssn.edu.in>
2021-11-03 21:08:48 +0530

    minor changes

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-const { app, BrowserWindow,globalShortcut,Tray, nativeImage } = require('electron')
-const path = require('path')
+const {
+  app,
+  BrowserWindow,
+  globalShortcut,
+  Tray,
+  nativeImage,
+} = require("electron");
+const path = require("path");
 
 let mainWindow;
 let eventWindow;
-let todoTray=null;
+let todoTray = null;
 
 function createEventWindow() {
   eventWindow = new BrowserWindow({
@@ -11,45 +17,48 @@ function createEventWindow() {
     height: 600,
     webPreferences: {
       nodeIntegration: true,
-      preload: path.join(__dirname, 'preload.js'),
-      enableRemoteModule: true
+      preload: path.join(__dirname, "preload.js"),
+      enableRemoteModule: true,
     },
     show: false,
-  })
-
-  eventWindow.addListener('close',(e)=>{
+  });
+  mainWindow.setMenu(null);
+  eventWindow.setMenu(null);
+  eventWindow.addListener("close", (e) => {
     eventWindow.hide();
     mainWindow.reload();
     e.preventDefault();
   });
 
   // console.log(path.join(__dirname, 'src/addEvent.html'))
-  eventWindow.loadFile(path.join(__dirname, 'src/addEvent.html'))
+  eventWindow.loadFile(path.join(__dirname, "src/addEvent.html"));
 }
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 420,
+    height: 450,
     webPreferences: {
-      nodeIntegration:true,
-      preload: path.join(__dirname, 'preload.js'),
-      enableRemoteModule:true
+      nodeIntegration: true,
+      preload: path.join(__dirname, "preload.js"),
+      enableRemoteModule: true,
     },
-    show:false
-  })
+    show: false,
+  });
 
-  mainWindow.addListener('close',(e)=>{
+  mainWindow.addListener("close", (e) => {
     // console.log(this.clearImmediate);
     mainWindow.hide();
     e.preventDefault();
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'src/todo/todo.html'))
+  mainWindow.loadFile(path.join(__dirname, "src/todo/todo.html"));
   // mainWindow.show();
-  mainWindow.once('ready-to-show',()=>{mainWindow.show()})
+  mainWindow.once("ready-to-show", () => {
+    mainWindow.show();
+  });
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -72,33 +81,39 @@ function createWindow () {
 //   })
 // })
 
-try{
-  app.whenReady().then(()=>{
-    createWindow();
-    createEventWindow();
-    todoTray=new Tray(nativeImage.createFromPath(path.join(__dirname,'icon.png')));
-    todoTray.on('click',()=>{
-      mainWindow.show();
+try {
+  app
+    .whenReady()
+    .then(() => {
+      createWindow();
+      createEventWindow();
+      todoTray = new Tray(
+        nativeImage.createFromPath(path.join(__dirname, "icon.png"))
+      );
+      todoTray.on("click", () => {
+        mainWindow.show();
+      });
+      globalShortcut.register("CommandOrControl+x", () => {
+        eventWindow.show();
+      });
+      app.on("activate", function () {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+      });
     })
-    globalShortcut.register('CommandOrControl+x',()=>{
-      eventWindow.show();
-    })
-    app.on('activate', function () {
-      // On macOS it's common to re-create a window in the app when the
-      // dock icon is clicked and there are no other windows open.
-      if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
-  }).catch((err)=>{
-    console.warn('Error',err);
-  })
+    .catch((err) => {
+      console.warn("Error", err);
+    });
+} finally {
+  // Quit when all windows are closed, except on macOS. There, it's common
+  // for applications and their menu bar to stay active until the user quits
+  // explicitly with Cmd + Q.
+  app.on("window-all-closed", function () {
+    if (process.platform !== "darwin") {
+    }
+  });
 }
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-finally{app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {}
-})}
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
--- src/addEvent.js
+++ src/addEvent.js
@@ -1,38 +1,36 @@
-const remote = require('electron').remote
-const fs=require('fs');
+const remote = require("electron").remote;
+const fs = require("fs");
 let win;
 
+const submit = document.querySelector("#btn");
 
-const submit = document.querySelector('#btn');
-
-submit.addEventListener('click', () => {
+submit.addEventListener("click", () => {
   win = remote.getCurrentWindow();
 
-  const title = document.querySelector('#title').value;
-  const message = document.querySelector('#message').value;
-  const date = document.querySelector('#date').value;
-  const time = document.querySelector('#time').value;
-  let content={title,message,date,time};
-  fs.readFile('tasks.json','utf-8',(err,data)=>{
+  const title = document.querySelector("#title").value;
+  const message = document.querySelector("#message").value;
+  const date = document.querySelector("#date").value;
+  const time = document.querySelector("#time").value;
+  let content = { title, message, date, time };
+  fs.readFile("tasks.json", "utf-8", (err, data) => {
     // console.log(data);
-    if(!data){
-      data={data:[]};
-      data=JSON.stringify(data);
+    if (!data) {
+      data = { data: [] };
+      data = JSON.stringify(data);
     }
-    let newjson=JSON.parse(data,null,2);
+    let newjson = JSON.parse(data, null, 2);
     console.log(newjson);
-    content['key']=newjson['data'].length;
-    newjson['data'].push(content);
-    content=newjson;
+    content["key"] = newjson["data"].length;
+    newjson["data"].push(content);
+    content = newjson;
     // console.log(newjson);
-    content=JSON.stringify(content);
+    content = JSON.stringify(content);
     console.log(content);
-    fs.writeFile('tasks.json', content, (err) => {
+    fs.writeFile("tasks.json", content, (err) => {
       if (err) {
-          throw err;
+        throw err;
       }
     });
-  })
-  // win.close();
+  });
+  win.close();
 });
-
